525fd8c750d701e269987d271f0eeef2a02e0f1a
Hikmet Sadigzade <sadigzade.hi@mail.ru>
2020-07-30 23:06:19 +0300

    Первый коммит: появились блоки страницы: шапка (header) и информация о странице (about). Прописаны все стили для этих блоков. Пока что без медиа-запросов. Медиа-запросы будут прописаны после всех появившихся блоков старинцы.

---
+++ js/script.min.js
@@ -0,0 +1,1 @@
+function testWebP(e){var A=new Image;A.onload=A.onerror=function(){e(2==A.height)},A.src="data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"}testWebP((function(e){1==e?document.querySelector("body").classList.add("webp"):document.querySelector("body").classList.add("no-webp")}));
